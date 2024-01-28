d5d4fef03fcf15d70dda3f7de8007de41aca8f5b
usmanov0 <azikusmanov882@gmail.com>
2024-01-28 21:08:11 +0500

    authenticated user details from gin context

---
+++ internal/helper/auth.go
@@ -0,0 +1,24 @@
+package helper
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"simple-crud-api/internal/middleware"
+)
+
+func GetAuthUser(c *gin.Context) *middleware.AuthUser {
+	authUser, exists := c.Get("authUser")
+
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to get the user",
+		})
+		return nil
+	}
+
+	if user, ok := authUser.(middleware.AuthUser); ok {
+		return &user
+	}
+
+	return nil
+}
