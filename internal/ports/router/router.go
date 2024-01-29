9b067549f431745204a64e002676ee774ed80a76
usmanov0 <azikusmanov882@gmail.com>
2024-01-29 17:27:43 +0500

    refactored :router.go

--- internal/ports/router/router.go
+++ internal/ports/router/router.go
@@ -3,45 +3,45 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"simple-crud-api/internal/middleware"
-	handler2 "simple-crud-api/internal/ports/handler"
+	handler "simple-crud-api/internal/ports/handler"
 )
 
 func Route(r *gin.Engine) {
-	r.POST("/api/sign-up", handler2.SignUp)
-	r.POST("/api/log-in", handler2.SignIn)
+	r.POST("/api/sign-up", handler.SignUp)
+	r.POST("/api/log-in", handler.SignIn)
 
 	r.Use(middleware.RequireAuth)
-	r.POST("/api/log-out", handler2.LogOut)
+	r.POST("/api/log-out", handler.LogOut)
 	userRouter := r.Group("/api/users")
 	{
-		userRouter.GET("/", handler2.GetUsers)
-		userRouter.PUT("/update", handler2.UpdateUser)
-		userRouter.DELETE("/delete", handler2.DeleteUser)
+		userRouter.GET("/", handler.GetUsers)
+		userRouter.PUT("/update", handler.UpdateUser)
+		userRouter.DELETE("/delete", handler.DeleteUser)
 	}
 
 	categoryRouter := r.Group("/api/categories")
 	{
-		categoryRouter.POST("/create", handler2.CreateCategory)
-		categoryRouter.GET("/", handler2.GetCategories)
-		categoryRouter.PUT("/update", handler2.UpdateCategory)
-		categoryRouter.DELETE("/delete", handler2.DeleteCategory)
+		categoryRouter.POST("/create", handler.CreateCategory)
+		categoryRouter.GET("/", handler.GetCategories)
+		categoryRouter.PUT("/update", handler.UpdateCategory)
+		categoryRouter.DELETE("/delete", handler.DeleteCategory)
 	}
 
 	postRouter := r.Group("/api/posts")
 	{
-		postRouter.POST("/create", handler2.CreatePost)
-		postRouter.GET("/", handler2.GetPosts)
-		postRouter.GET("/read-post", handler2.ReadPosts)
-		postRouter.GET("/edit", handler2.EditPost)
-		postRouter.PUT("/update", handler2.UpdatePost)
-		postRouter.DELETE("/delete", handler2.DeletePost)
+		postRouter.POST("/create", handler.CreatePost)
+		postRouter.GET("/", handler.GetPosts)
+		postRouter.GET("/read-post", handler.ReadPosts)
+		postRouter.GET("/edit", handler.EditPost)
+		postRouter.PUT("/update", handler.UpdatePost)
+		postRouter.DELETE("/delete", handler.DeletePost)
 	}
 
-	commentRouter := r.Group("/api/comment")
+	commentRouter := r.Group("/api/comments")
 	{
-		commentRouter.POST("/comment", handler2.CommentOnPost)
-		commentRouter.PUT("/update", handler2.UpdateComment)
-		commentRouter.DELETE("/delete", handler2.DeleteComment)
+		commentRouter.POST("/comment", handler.CommentOnPost)
+		commentRouter.PUT("/update", handler.UpdateComment)
+		commentRouter.DELETE("/delete", handler.DeleteComment)
 	}
 
 }
