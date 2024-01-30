88625b36393025dbcb755c9cce210cb30d9a4dfd
usmanov0 <azikusmanov882@gmail.com>
2024-01-31 01:13:47 +0500

    added :Category response

--- models/category_model.go
+++ models/category_model.go
@@ -1,10 +1,17 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"simple-crud-api/pkg/pagination"
+)
 
 type Category struct {
 	gorm.Model
 	Name  string `gorm:"column:name;type:varchar(255);unique;not null" json:"name"`
 	Slug  string `gorm:"column:slug;type:varchar(255);unique;not null" json:"slug"`
 	Posts []Post `gorm:"foreignKey:CategoryId" json:"posts"`
+}
+
+type GetCategoriesResponse struct {
+	Response pagination.PaginateRes `json:"response"`
 }
