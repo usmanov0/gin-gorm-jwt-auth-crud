569a399796a59f20863069e01418070aca8c4fe2
usmanov0 <azikusmanov882@gmail.com>
2024-01-31 00:55:17 +0500

    added :request and responses

--- models/user_model.go
+++ models/user_model.go
@@ -8,3 +8,32 @@ type User struct {
 	Email    string `gorm:"column:email;type:varchar(255);unique;not null" json:"email"`
 	Password string `gorm:"column:password;type:varchar(255);not null" json:"-"`
 }
+
+type SignInRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UpdateUserRequest struct {
+	Name  string `json:"name" binding:"required,min=2,max=50"`
+	Email string `json:"email" binding:"required,email"`
+}
+
+type SignUpRequest struct {
+	Name     string `json:"name" binding:"required,min=2,max=50"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type GetUserResponse struct {
+	Users []User `json:"users"`
+}
+
+type UpdateRequest struct {
+	Name  string `json:"name" binding:"required,min=2,max=50"`
+	Email string `json:"email" binding:"required,email"`
+}
+
+type UpdateResponse struct {
+	User User `json:"user"`
+}
