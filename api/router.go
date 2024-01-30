1f7ffccfa8d21d1ff6ead9d722c4323b86196573
usmanov0 <azikusmanov882@gmail.com>
2024-01-30 11:37:04 +0500

    moved :to api

---
+++ api/router.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"simple-crud-api/controller"
+	"simple-crud-api/middleware"
+)
+
+func Route(r *gin.Engine) {
+	r.POST("/api/sign-up", controller.SignUp)
+	r.POST("/api/log-in", controller.SignIn)
+
+	r.Use(middleware.RequireAuth)
+	r.POST("/api/log-out", controller.LogOut)
+	userRouter := r.Group("/api/users")
+	{
+		userRouter.GET("/", controller.GetUsers)
+		userRouter.PUT("/update", controller.UpdateUser)
+		userRouter.DELETE("/delete", controller.DeleteUser)
+	}
+
+	categoryRouter := r.Group("/api/categories")
+	{
+		categoryRouter.POST("/create", controller.CreateCategory)
+		categoryRouter.GET("/", controller.GetCategories)
+		categoryRouter.PUT("/update", controller.UpdateCategory)
+		categoryRouter.DELETE("/delete", controller.DeleteCategory)
+	}
+
+	postRouter := r.Group("/api/posts")
+	{
+		postRouter.POST("/create", controller.CreatePost)
+		postRouter.GET("/", controller.GetPosts)
+		postRouter.GET("/read-post", controller.ReadPosts)
+		postRouter.GET("/edit", controller.EditPost)
+		postRouter.PUT("/update", controller.UpdatePost)
+		postRouter.DELETE("/delete", controller.DeletePost)
+	}
+
+	commentRouter := r.Group("/api/comments")
+	{
+		commentRouter.POST("/comment", controller.CommentOnPost)
+		commentRouter.PUT("/update", controller.UpdateComment)
+		commentRouter.DELETE("/delete", controller.DeleteComment)
+	}
+
+}
