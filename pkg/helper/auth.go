bf8e1ee5135ca43446aaa87f9eb8365b2dfc58eb
usmanov0 <azikusmanov882@gmail.com>
2024-01-30 22:39:22 +0500

    added :to return error

--- pkg/helper/auth.go
+++ pkg/helper/auth.go
@@ -1,24 +1,25 @@
 package helper
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"simple-crud-api/middleware"
 )
 
-func GetAuthUser(c *gin.Context) *middleware.AuthUser {
+func GetAuthUser(c *gin.Context) (*middleware.AuthUser, error) {
 	authUser, exists := c.Get("authUser")
 
 	if !exists {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": "Failed to get the user",
 		})
-		return nil
+		return nil, fmt.Sprintf("Failed to get user")
 	}
 
 	if user, ok := authUser.(middleware.AuthUser); ok {
-		return &user
+		return &user, nil
 	}
 
-	return nil
+	return nil, nil
 }
